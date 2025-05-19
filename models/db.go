306d37715a2dcf2816880f3fb11fda6bd207a1bf
Jimyeong <idjjm92@gmail.com>
2025-05-19 18:40:13 +0100

    db connection str

--- models/db.go
+++ models/db.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 	"sync"
-	
+
 	"github.com/jimyeongjung/owlverload_api/utils"
 )
 
@@ -33,29 +33,30 @@ func NewSQLDB(dbConfig DBConfig) *SQLDB {
 	defer utils.Trace()()
 
 	utils.Info("Connecting to database: %s:%s/%s", dbConfig.DB_HOST, dbConfig.DB_PORT, dbConfig.DB_NAME)
-	
+
 	// Log connection string without password for security
-	connStrRedacted := fmt.Sprintf("%s:***@tcp(%s:%s)/%s?parseTime=true", 
+	connStrRedacted := fmt.Sprintf("%s:***@tcp(%s:%s)/%s?parseTime=true",
 		dbConfig.DB_USER, dbConfig.DB_HOST, dbConfig.DB_PORT, dbConfig.DB_NAME)
 	utils.Debug("Connection string: %s", connStrRedacted)
-	
+
 	// Actual connection string with password
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", 
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		dbConfig.DB_USER, dbConfig.DB_PASSWORD, dbConfig.DB_HOST, dbConfig.DB_PORT, dbConfig.DB_NAME)
-	
+	fmt.Println("@@@@@@@@@@@@@@@@@@---connStr---", connStr)
+
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		utils.Error("Failed to open database connection: %v", err)
 		log.Fatal(err)
 		return &SQLDB{DB: nil, Err: err}
 	}
-	
+
 	// Test the connection
 	if err := db.Ping(); err != nil {
 		utils.Error("Database ping failed: %v", err)
 		return &SQLDB{DB: nil, Err: err}
 	}
-	
+
 	utils.Info("Database connected successfully")
 	return &SQLDB{DB: db, Err: nil}
 }
@@ -63,7 +64,7 @@ func NewSQLDB(dbConfig DBConfig) *SQLDB {
 // Enhanced Query method with logging
 func (s *SQLDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
 	utils.TraceSQL(query, args...)
-	
+
 	rows, err := s.DB.Query(query, args...)
 	if err != nil {
 		utils.Error("Query failed: %v\nQuery: %s\nArgs: %v", err, query, args)
@@ -80,7 +81,7 @@ func (s *SQLDB) QueryRow(query string, args ...interface{}) *sql.Row {
 // Enhanced Exec method with logging
 func (s *SQLDB) Exec(query string, args ...interface{}) (sql.Result, error) {
 	utils.TraceSQL(query, args...)
-	
+
 	result, err := s.DB.Exec(query, args...)
 	if err != nil {
 		utils.Error("Exec failed: %v\nQuery: %s\nArgs: %v", err, query, args)
@@ -96,7 +97,7 @@ func (s *SQLDB) Exec(query string, args ...interface{}) (sql.Result, error) {
 // Enhanced Prepare method with logging
 func (s *SQLDB) Prepare(query string) (*sql.Stmt, error) {
 	utils.Debug("Preparing SQL statement: %s", query)
-	
+
 	stmt, err := s.DB.Prepare(query)
 	if err != nil {
 		utils.Error("Prepare failed: %v\nQuery: %s", err, query)
@@ -106,7 +107,7 @@ func (s *SQLDB) Prepare(query string) (*sql.Stmt, error) {
 
 func GetDBConfig() DBConfig {
 	utils.Debug("Getting database configuration from environment variables")
-	
+
 	config := DBConfig{
 		DB_USER:     os.Getenv("DB_USER"),
 		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
@@ -114,14 +115,14 @@ func GetDBConfig() DBConfig {
 		DB_PORT:     os.Getenv("DB_PORT"),
 		DB_NAME:     os.Getenv("DB_NAME"),
 	}
-	
+
 	// Verify that we have all required configuration
 	if config.DB_USER == "" || config.DB_HOST == "" || config.DB_PORT == "" || config.DB_NAME == "" {
 		utils.Warn("Incomplete database configuration: User=%s, Host=%s, Port=%s, Name=%s",
-			maskEmpty(config.DB_USER), maskEmpty(config.DB_HOST), 
+			maskEmpty(config.DB_USER), maskEmpty(config.DB_HOST),
 			maskEmpty(config.DB_PORT), maskEmpty(config.DB_NAME))
 	}
-	
+
 	return config
 }
 
@@ -135,15 +136,15 @@ func maskEmpty(s string) string {
 
 func GetDBInstance(dbConfig DBConfig) *sql.DB {
 	utils.Debug("Getting database instance")
-	
+
 	once.Do(func() {
 		utils.Info("Initializing database instance (first call)")
 		dbInstance = NewSQLDB(dbConfig).DB
 	})
-	
+
 	if dbInstance == nil {
 		utils.Error("Database instance is nil - initialization may have failed")
 	}
-	
+
 	return dbInstance
 }
